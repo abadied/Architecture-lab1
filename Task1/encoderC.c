7701feabfee77155a831fee703183a4edfee8be3
abadied <abadi.eden@gmail.com>
2017-03-22 14:23:42 +0200

    hey

---
+++ Task1/encoderC.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char ** argv){
+    FILE * output = stdout;
+    FILE* input = stdin;
+    int ver = 0;
+    int number;
+    for(int i = 1; i < argc; i++){
+            if(strcmp(argv[i], "-i") == 0){
+                input = fopen(argv[++i],"r");
+            }
+            else if(strcmp(argv[i],"-o") == 0){
+            output = fopen(argv[++i],"w");
+            }
+            else if(argv[i][0] == '+'){
+                ver =1;
+            }
+            else if(argv[i][0] == '-'){
+                ver = 2;
+            }
+            else{
+                printf("invalid parameter - %s\n",argv[i]);
+                return 1;
+            }
+            
+    }
+    if(ver != 0)
+        number = atoi(argv[1] + 1);
+    int c = fgetc(input);
+    while (c != EOF){
+        if(c <= 'z' && c >= 'A'){
+            if(c <= 'Z')
+                c = c | (1 << 5);
+            if(ver == 1){
+                        c = c + number;
+            }
+            else{
+                    c = c - number;
+            }
+        }
+        fputc(c, output);
+        c = fgetc(input);
+    }
+}
