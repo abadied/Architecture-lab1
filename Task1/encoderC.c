10b3858faac567502138114f7f06db7436dddb8a
abadied <abadi.eden@gmail.com>
2017-03-22 15:41:13 +0200

    finished

--- Task1/encoderC.c
+++ Task1/encoderC.c
@@ -7,12 +7,18 @@ int main(int argc, char ** argv){
     FILE* input = stdin;
     int ver = 0;
     int number;
+    char s[31];
     for(int i = 1; i < argc; i++){
             if(strcmp(argv[i], "-i") == 0){
                 input = fopen(argv[++i],"r");
             }
             else if(strcmp(argv[i],"-o") == 0){
-            output = fopen(argv[++i],"w");
+                printf("Enter output file name:");
+                fgets(s,30,stdin);
+                char* pos = strchr(s,'\n');
+                if(pos != NULL)
+                    *pos = '\0';
+                output = fopen(s,"w");
             }
             else if(argv[i][0] == '+'){
                 ver =1;
@@ -30,13 +36,13 @@ int main(int argc, char ** argv){
         number = atoi(argv[1] + 1);
     int c = fgetc(input);
     while (c != EOF){
-        if(c <= 'z' && c >= 'A'){
-            if(c <= 'Z')
+        if((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A')){
+            if(c <= 'Z' && c >='A')
                 c = c | (1 << 5);
             if(ver == 1){
                         c = c + number;
             }
-            else{
+            else if(ver == 2){
                     c = c - number;
             }
         }
